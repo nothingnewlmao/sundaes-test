aab7c579b35565b5096415cf64a624c9083510e7
Kirsanova.VA <Kirsanova.VA@gazprom-neft.ru>
2022-12-16 21:18:18 +0400

    tests for grand total

--- src/pages/entry/OrderEntry.tsx
+++ src/pages/entry/OrderEntry.tsx
@@ -1,11 +1,17 @@
 import {FC} from "react";
 import Options, {EOptionType} from "./Options";
+import {formatCurrency} from "../../utilities";
+import {useOrderDetails} from "../../contexts/OrderDetails";
 
-const OrderEntry: FC = () => (
-  <div>
-    <Options optionType={EOptionType.scoops} />
-    <Options optionType={EOptionType.toppings} />
+const OrderEntry: FC = () => {
+  const { totals } = useOrderDetails()
+  const grandTotal = totals[EOptionType.scoops] + totals[EOptionType.toppings]
+
+  return <div>
+    <h2>Grand total: {formatCurrency(grandTotal)}</h2>
+    <Options optionType={EOptionType.scoops}/>
+    <Options optionType={EOptionType.toppings}/>
   </div>
-)
+}
 
 export default OrderEntry
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,42 +1,101 @@
 import { render, screen } from "../../../test-utils/test-utils";
 import userEvent from "@testing-library/user-event";
 import Options, {EOptionType} from "../Options";
+import OrderEntry from "../OrderEntry";
 
 describe('Total', () => {
-  test('update subtotal when scoops change', async () => {
-    render(<Options optionType={EOptionType.scoops} />)
+  describe('Subtotals', () => {
+    test('update subtotal when scoops change', async () => {
+      render(<Options optionType={EOptionType.scoops} />)
 
-    const scoopSubtotal = screen.getByText('Scoops total:', { exact: false })
-    expect(scoopSubtotal).toHaveTextContent('$0.00')
+      const scoopSubtotal = screen.getByText('Scoops total:', { exact: false })
+      expect(scoopSubtotal).toHaveTextContent('$0.00')
 
-    const vanillaScoop = await screen.findByRole('spinbutton', { name: 'vanilla' })
-    const userEvents = userEvent.setup()
-    await userEvents.clear(vanillaScoop)
-    await userEvents.type(vanillaScoop, '1')
-    expect(scoopSubtotal).toHaveTextContent('$2.00')
+      const vanillaScoop = await screen.findByRole('spinbutton', { name: 'vanilla' })
+      const userEvents = userEvent.setup()
+      await userEvents.clear(vanillaScoop)
+      await userEvents.type(vanillaScoop, '1')
+      expect(scoopSubtotal).toHaveTextContent('$2.00')
 
-    const chocolateInput = await screen.findByRole('spinbutton', { name: 'chocolate' })
-    await userEvents.clear(chocolateInput)
-    await userEvents.type(chocolateInput, '2')
-    expect(scoopSubtotal).toHaveTextContent('$6.00')
+      const chocolateInput = await screen.findByRole('spinbutton', { name: 'chocolate' })
+      await userEvents.clear(chocolateInput)
+      await userEvents.type(chocolateInput, '2')
+      expect(scoopSubtotal).toHaveTextContent('$6.00')
+    })
+
+    test('updates subtotal when toppings updates', async() => {
+      const user = userEvent.setup()
+      render(<Options optionType={EOptionType.toppings} />)
+
+      const subtotalTitle = screen.getByText('Toppings total:', { exact: false })
+      expect(subtotalTitle).toHaveTextContent('$0.00')
+
+      const cherriesChkbx = await screen.findByRole('checkbox', { name: 'cherries'})
+      await user.click(cherriesChkbx)
+      expect(subtotalTitle).toHaveTextContent('$1.50')
+
+      const mmChkbx = await screen.findByRole('checkbox', { name: 'm&m'})
+      await user.click(mmChkbx)
+      expect(subtotalTitle).toHaveTextContent('$3.00')
+
+      await user.click(cherriesChkbx)
+      expect(subtotalTitle).toHaveTextContent('$1.50')
+    })
   })
 
-  test('updates subtotal when toppings updates', async() => {
-    const user = userEvent.setup()
-    render(<Options optionType={EOptionType.toppings} />)
+  describe('Grand total', () => {
+    test('grand total is zero', () => {
+      render(<OrderEntry />)
 
-    const subtotalTitle = screen.getByText('Toppings total:', { exact: false })
-    expect(subtotalTitle).toHaveTextContent('$0.00')
+      const totalTitle = screen.getByRole('heading', { name: /grand total: /i })
 
-    const cherriesChkbx = await screen.findByRole('checkbox', { name: 'cherries'})
-    await user.click(cherriesChkbx)
-    expect(subtotalTitle).toHaveTextContent('$1.50')
+      expect(totalTitle).toHaveTextContent('$0.00')
+    })
 
-    const mmChkbx = await screen.findByRole('checkbox', { name: 'm&m'})
-    await user.click(mmChkbx)
-    expect(subtotalTitle).toHaveTextContent('$3.00')
+    test('grand total updates properly if scoop is added first', async () => {
+      const user = userEvent.setup()
+      render(<OrderEntry />)
 
-    await user.click(cherriesChkbx)
-    expect(subtotalTitle).toHaveTextContent('$1.50')
+      const chocolateInput = await screen.findByRole('spinbutton', { name: /chocolate/i })
+      const grandTotalTitle = screen.getByRole('heading', { name: /grand total/i })
+
+      await user.clear(chocolateInput)
+      await user.type(chocolateInput, '2')
+
+      expect(grandTotalTitle).toHaveTextContent('$4.00')
+    })
+
+    test('grand total updates properly if topping is added first', async() => {
+      const user = userEvent.setup()
+      render(<OrderEntry />)
+
+      const grandTotalTitle = screen.getByRole('heading', { name: /grand total/i })
+      const cherriesChkbx = await screen.findByRole('checkbox', { name: 'cherries' })
+
+      await user.click(cherriesChkbx)
+
+      expect(grandTotalTitle).toHaveTextContent('$1.50')
+    })
+
+    test('grand total updates properly if an item is removed', async() => {
+      const user = userEvent.setup()
+      render(<OrderEntry />)
+
+      const grandTotalTitle = screen.getByRole('heading', { name: /grand total/i })
+      const mmChkbx = await screen.findByRole('checkbox', { name: 'm&m' })
+      const vanillaInput = await screen.findByRole('spinbutton', { name: 'vanilla' })
+
+      await user.clear(vanillaInput)
+      await user.click(mmChkbx)
+      await user.type(vanillaInput, '4')
+      expect(grandTotalTitle).toHaveTextContent('$9.50')
+
+      await user.click(mmChkbx)
+      expect(grandTotalTitle).toHaveTextContent('$8.00')
+
+      await user.clear(vanillaInput)
+      await user.type(vanillaInput, '0')
+      expect(grandTotalTitle).toHaveTextContent('$0.00')
+    })
   })
 })
