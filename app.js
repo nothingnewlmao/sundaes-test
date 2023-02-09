228b6f3cb717f45a69fc067772fad92e789f5c8e
Kirsanova.VA <Kirsanova.VA@gazprom-neft.ru>
2023-02-09 23:03:16 +0400

    fixed port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // express server on port 5000
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app
   .use(express.static(__dirname + '/dist'))
